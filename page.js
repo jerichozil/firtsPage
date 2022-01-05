775e10451c252b83f671fbc55d0b47f27fc90dcf
jerichozil <34137959+jerichozil@users.noreply.github.com>
2022-01-05 14:03:46 +0200

    Add files via upload

---
+++ page.js
@@ -0,0 +1,5 @@
+const showDropMenu = () => {
+	document.querySelector(".dropMenu").classList.toggle("hide");
+}
+
+// const <function name> = ( parameters ) => { function body recieves the parameters };
